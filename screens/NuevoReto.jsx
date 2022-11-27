126212bf6c9ddb8b9c5d989152ae795e6d91f978
Gerard Siles <gerardsiles@gmail.com>
2022-11-27 11:50:09 +0000

    React-native-elements input, Icon, Button

--- screens/NuevoReto.jsx
+++ screens/NuevoReto.jsx
@@ -4,17 +4,17 @@ import {
 	ActivityIndicator,
 	TouchableOpacity,
 	TextInput,
-	Button,
 	TouchableWithoutFeedback,
 	Keyboard,
 	KeyboardAvoidingView,
-	StyleSheet,
 } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { useTailwind } from 'tailwind-rn/dist';
-
 import { useLayoutEffect, useState } from 'react';
-import { Image } from '@rneui/themed';
+import { Input, Button } from '@rneui/themed';
 import { AntDesign } from '@expo/vector-icons';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import { db } from '../db/firebaseConfig';
@@ -32,6 +32,14 @@ const NuevoReto = () => {
 	const [periodicidad, setPeriodicidad] = useState('');
 	const [activo, setActivo] = useState(false);
 	const [completado, setCompletado] = useState('');
+
+	const nameRef = React.createRef();
+	const detalleRef = React.createRef();
+	const categoriaRef = React.createRef();
+	const tiempoRef = React.createRef();
+	const periodicidadRef = React.createRef();
+	const prioridadRef = React.createRef();
+	const completadoRef = React.createRef();
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
@@ -64,13 +72,15 @@ const NuevoReto = () => {
 	}, [navigation]);
 
 	const handleSubmit = () => {
-		if (
-			name.length < 1 ||
-			detalle.length < 1 ||
-			categoria.length < 1 ||
-			prioridad.length < 1
-		) {
-			return alert('No pueden haber campos vacios');
+		if (name.length < 1) {
+			nameRef.current.shake();
+			nameRef.current.setNativeProps({
+				errorMessage: 'No puede estar vacio',
+				renderErrorMessage: true,
+			});
+		}
+		if (detalle.length < 1 || categoria.length < 1 || prioridad.length < 1) {
+			detalleRef.current.shake();
 		} else {
 			addDoc(collection(db, 'retos'), {
 				activo: activo,
@@ -88,62 +98,87 @@ const NuevoReto = () => {
 	};
 
 	return (
-		<KeyboardAvoidingView
+		<KeyboardAwareScrollView
 			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
 			style={tw('flex-1')}
 		>
 			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
 				<View
-					style={[tw('flex-1 justify-around'), { backgroundColor: '#998830' }]}
+					style={[tw('flex-1 justify-around'), { backgroundColor: '#ffffff' }]}
 				>
 					<View style={tw('items-center')}>
-						<Text style={tw('font-bold text-white text-2xl mt-3 ')}>
-							Nuevo Reto
-						</Text>
+						<Text style={tw('font-bold text-2xl mt-3 ')}>Nuevo Reto</Text>
 					</View>
-					<View style={tw('ml-3')}>
-						<Text style={tw('text-white text-xl mt-2 ')}>Nombre</Text>
-						<TextInput
-							style={tw('underline decoration-slate-200 text-lg mb-1')}
-							placeholder='nombre'
+					<View style={tw('ml-3 mr-3')}>
+						<Text style={tw('text-xl mt-2')}>Nombre</Text>
+						<Input
+							returnKeyType='next'
+							placeholder='titulo de tu nuevo reto'
 							onChangeText={setName}
+							onSubmitEditing={() => {
+								detalleRef.current.focus();
+							}}
+							ref={nameRef}
+							errorMessage=''
+							renderErrorMessage={false}
 						/>
-						<Text style={tw('text-white text-xl mt-2 ')}>Detalle</Text>
-						<TextInput
-							style={tw('underline decoration-slate-200 text-lg mb-1')}
+						<Text style={tw('text-xl  mt-2')}>Detalle</Text>
+						<Input
+							returnKeyType='next'
+							ref={detalleRef}
 							placeholder='detalle'
 							onChangeText={setDetalle}
+							onSubmitEditing={() => {
+								categoriaRef.current.focus();
+							}}
 						/>
-						<Text style={tw('text-white text-xl mt-2 ')}>Categoria</Text>
-						<TextInput
-							style={tw('underline decoration-slate-200 text-lg mb-1')}
+						<Text style={tw('text-xl mt-2')}>Categoria</Text>
+						<Input
+							returnKeyType='next'
+							ref={categoriaRef}
 							placeholder='categoria'
 							onChangeText={setCategoria}
+							onSubmitEdtiting={() => {
+								tiempoRef.current.focus();
+							}}
 						/>
-						<Text style={tw('text-white text-xl mt-2 ')}>Tiempo</Text>
-						<TextInput
-							style={tw('underline decoration-slate-200 text-lg mb-1')}
+						<Text style={tw('text-xl mt-2')}>Tiempo</Text>
+						<Input
+							returnKeyType='next'
+							ref={tiempoRef}
 							placeholder='tiempo'
 							onChangeText={setTiempo}
 							keyboardType='number-pad'
+							onSubmitEditing={() => {
+								periodicidadRef.current.focus();
+							}}
 						/>
-						<Text style={tw('text-white text-xl mt-2 ')}>Periodicidad</Text>
-						<TextInput
-							style={tw('underline decoration-slate-200 text-lg mb-1')}
+						<Text style={tw('text-xl mt-2 ')}>Periodicidad</Text>
+						<Input
+							returnKeyType='next'
+							ref={periodicidadRef}
 							placeholder='periodicidad'
 							onChangeText={setPeriodicidad}
 							keyboardType='number-pad'
+							onSubmitEditing={() => {
+								prioridadRef.current.focus();
+							}}
 						/>
-						<Text style={tw('text-white text-xl mt-2 ')}>Prioridad</Text>
-						<TextInput
+						<Text style={tw('text-xl mt-2 ')}>Prioridad</Text>
+						<Input
+							returnKeyType='next'
+							ref={prioridadRef}
 							style={tw('underline decoration-slate-200 text-lg mb-1')}
 							placeholder='prioridad'
 							onChangeText={setPrioridad}
+							onSubmitEditing={() => {
+								completadoRef.current.focus();
+							}}
 						/>
 
-						<Text style={tw('text-white text-xl mt-2 ')}>Completado</Text>
-						<TextInput
-							style={tw('underline decoration-slate-200 text-lg mb-1')}
+						<Text style={tw('text-xl mt-2 ')}>Completado</Text>
+						<Input
+							ref={completadoRef}
 							placeholder='completado'
 							onChangeText={setCompletado}
 							keyboardType='number-pad'
@@ -154,15 +189,17 @@ const NuevoReto = () => {
 								onPress={() => setActivo(!activo)}
 								style={{ marginTop: 6 }}
 							/>
-							<Text style={tw('text-white text-xl mt-2 ')}>Activo</Text>
+							<Text style={tw('text-xl mt-2 ')}>Activo</Text>
 						</View>
 					</View>
-					<View style={tw('m-5')}>
-						<Button title='Guardar' color={'#fff'} onPress={handleSubmit} />
+					<View style={tw('m-5 rounded-lg items-center mb-5')}>
+						<Icon.Button name='save' onPress={handleSubmit}>
+							Guardar
+						</Icon.Button>
 					</View>
 				</View>
 			</TouchableWithoutFeedback>
-		</KeyboardAvoidingView>
+		</KeyboardAwareScrollView>
 	);
 };
 
